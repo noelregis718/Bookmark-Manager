417948415ad8adf8735b1c038ef3b25c68ae31d3
Noel Regis <noelregis718@gmail.com>
2025-05-21 22:55:43 +0530

    Delete redux-toolkit directory

--- redux-toolkit/src/components/index.ts
+++
@@ -1,5 +0,0 @@
-import BookmarkForm from "./BookmarkForm";
-import BookmarkItem from "./BookmarkItem";
-import BookmarkList from "./BookmarkList";
-
-export { BookmarkForm, BookmarkItem, BookmarkList };
